e49d9a66198f410545cab845456c6e014a32af22
LuizFelipe99 <luizfelipetomaz30@gmail.com>
2025-12-19 15:09:21 -0300

    add clock icon

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { NgFor, NgIf } from '@angular/common';
+import { NgFor, NgIf, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-home',
